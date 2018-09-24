7526c9499a91e28219f7ab187ae943b12059ae41
Gravitana <v.a.grahov@yandex.ru>
2018-09-24 11:03:54 +0500

    Локализация

---
+++ resources/lang/en/lng.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by Gravitana
+ */
+
+return [
+    
+    'site_title'                    => "Registration by invitation",
+    'company_group'                 => "Group of companies «Galion»",
+    'company_group_board'           => "Information board of group of companies «Galion»",
+    'welcome'                       => "Welcome, :Name",
+
+    // Error pages
+    '403_message'                   => "Sorry, you are not authorized to access this page.",
+    '404_message'                   => "Sorry, the page you are looking for could not be found.",
+    '419_message'                   => "Sorry, your session has expired. Please refresh and try again.",
+    '429_message'                   => "Sorry, you are making too many requests to our servers.",
+    '500_message'                   => "Whoops, something went wrong on our servers.",
+    '503_message'                   => "Sorry, we are doing some maintenance. Please check back soon.",
+    
+    // Verify Email
+    'verify' => [
+        'title'                     => "Verify Your Email Address",
+        'has_been_sent'             => "A fresh verification link has been sent to your email address.",
+        'check_email'               => "Before proceeding, please check your email for a verification link.",
+        'did_not_receive'           => "If you did not receive the email",
+        'request_another'           => "click here to request another",
+    ],
+    
+
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ config('app.locale') }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -63,32 +63,27 @@
                 margin-bottom: 30px;
             }
         </style>
+
+        <!-- SweetAlert2 -->
+        <script src="{{ asset('vendor/sweetalert2/sweetalert2.all.min.js') }}"></script>
+        <link href="{{ asset('vendor/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">{{ __('Home') }}</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a href="{{ route('register') }}">{{ __('Register') }}</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    {{ __('lng.site_title') }}
                 </div>
             </div>
         </div>
